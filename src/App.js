91ba2c5bf53ac896632217672f6ed3b513ded74c
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-06-17 23:56:48 -0500

    Handle checkbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState,useReduceruseReduceruseReducer} from 'react';
 
 function App() {
   const [name,setName]=useState(
     {
       nombre:'',
+      terms:false
     }
   );
 
@@ -15,16 +16,31 @@ function App() {
     })
   }
 
-  const enviarDatos = (event) => {
+  const handleInputClick = (event) => {
+    setName({
+        ...name,
+        [event.target.name] : event.target.checked
+    })
+  }
+
+  const sendData = (event) => {
     event.preventDefault()
-    alert("Hola "+name.nombre)
+    if(name.terms===true)
+      alert("Hola "+name.nombre)
+    else
+      alert("Accept Terms")
   }
 
   return (
     <div className="App">
       <h1>Hola Mundo</h1>
-      <form onSubmit={enviarDatos}>
+      <form onSubmit={sendData}>
         <input type="text" placeholder="Nombre" onChange={handleInputChange} name="nombre"></input>
+        <div>
+          <label>
+            <input type="checkbox"  onClick={handleInputClick} name="terms"/> Acepto los términos
+          </label>
+        </div>
         <button type="submit">Say Hi!</button>
       </form>
     </div>
